ef10d5d268488563656d90a745a9ab20bfac4faf
Benjamin Mbambi Pedro <benjamin.mbambi-pedro@epitech.eu>
2023-06-01 14:35:55 +0200

    ajout test du portefolio

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const { Container } = require('postcss');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:['poppins','sans-serif']
+      },
+      container:{
+        center:true,
+        padding:'1rem',
+        screens:{
+          'sm' : "512px",
+          'md' : "720px",
+          'lg' : "1024px",
+          'xl' : "1140px",
+          '2xl' : "1320px",
+        }
+      },
+      backgroundImage:{
+        'hero-img':"url('/src/images/moon2.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
